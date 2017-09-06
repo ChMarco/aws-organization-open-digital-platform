6c9cbade38aa9f55d6b0fc665421315e2999dd04
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-06 16:30:21 +0200

    troubleshooting

--- terraform/infrastructure/management-infrastructure/70-codebuild.tf
+++ terraform/infrastructure/management-infrastructure/70-codebuild.tf
@@ -1,23 +1,23 @@
-//module "codebuild_jenkins" {
-//  source = "../../modules/codebuild"
-//
-//  vpc_shortname = "${var.vpc_shortname}"
-//  aws_region = "${var.aws_region}"
-//
-//  codebuild_name = "${var.jenkins_codebuild_name}"
-//  codebuild_repo = "${var.jenkins_codebuild_repo}"
-//  codebuild_ecr = "${var.jenkins_codebuild_ecr}"
-//  image_name = "${var.jenkins_image_name}"
-//}
-//
-//module "codebuild_jenkins_slave" {
-//  source = "../../modules/codebuild"
-//
-//  vpc_shortname = "${var.vpc_shortname}"
-//  aws_region = "${var.aws_region}"
-//
-//  codebuild_name = "${var.jenkins_slave_codebuild_name}"
-//  codebuild_repo = "${var.jenkins_slave_codebuild_repo}"
-//  codebuild_ecr = "${var.jenkins_slave_codebuild_ecr}"
-//  image_name = "${var.jenkins_slave_image_name}"
-//}
+module "codebuild_jenkins" {
+  source = "../../modules/codebuild"
+
+  vpc_shortname = "${var.vpc_shortname}"
+  aws_region = "${var.aws_region}"
+
+  codebuild_name = "${var.jenkins_codebuild_name}"
+  codebuild_repo = "${var.jenkins_codebuild_repo}"
+  codebuild_ecr = "${var.jenkins_codebuild_ecr}"
+  image_name = "${var.jenkins_image_name}"
+}
+
+module "codebuild_jenkins_slave" {
+  source = "../../modules/codebuild"
+
+  vpc_shortname = "${var.vpc_shortname}"
+  aws_region = "${var.aws_region}"
+
+  codebuild_name = "${var.jenkins_slave_codebuild_name}"
+  codebuild_repo = "${var.jenkins_slave_codebuild_repo}"
+  codebuild_ecr = "${var.jenkins_slave_codebuild_ecr}"
+  image_name = "${var.jenkins_slave_image_name}"
+}
