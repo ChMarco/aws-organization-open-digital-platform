b1f4a7cc53612392e22fd1c30e8ac13b4cbd3f50
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-11 18:52:57 +0100

    standard infra

--- terraform/infrastructure/standard-infrastructure/99-outputs.tf
+++ terraform/infrastructure/standard-infrastructure/99-outputs.tf
@@ -9,15 +9,3 @@ output "cloudtrail_outputs" {
 output "bastion_outputs" {
   value = "${module.bastion.bastion_outputs}"
 }
-
-output "jenkins_outputs" {
-  value = "${module.jenkins.jenkins_outputs}"
-}
-
-//output "bitbucket_outputs" {
-//  value = "${module.bitbucket.bitbucket_outputs}"
-//}
-
-//output "openvpn_outputs" {
-//  value = "${module.openvpn.openvpn_outputs}"
-//}
