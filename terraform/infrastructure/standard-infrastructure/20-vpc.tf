05c4dbc0c5dec9a79ba87a717a07623a4b3bb8ab
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-14 18:53:59 +0100

    module fixing

--- terraform/infrastructure/standard-infrastructure/20-vpc.tf
+++ terraform/infrastructure/standard-infrastructure/20-vpc.tf
@@ -21,29 +21,29 @@ module "vpc" {
 
 }
 
-module "vpc_peering" {
-  source = "../../modules/vpc-peering"
-
-  mgmt_vpc_id = "${var.mgmt_vpc_id}"
-  mgmt_vpc_cidr = "${var.mgmt_vpc_cidr}"
-  mgmt_vpc_shortname = "${var.mgmt_vpc_shortname}"
-
-  vpc_id = "${lookup(module.vpc.vpc_outputs, "vpc_id")}"
-  vpc_cidr = "${var.vpc_cidr_block}"
-  vpc_shortname = "${var.vpc_shortname}"
-
-  dmz_route_tables = "${lookup(module.vpc.vpc_outputs, "dmz_rt_id")}"
-  public_route_tables = "${lookup(module.vpc.vpc_outputs, "public_rt_id")}"
-  private_route_tables = "${lookup(module.vpc.vpc_outputs, "private_rt_id")}"
-
-  deploy_environment = "${var.deploy_environment}"
-
-  tag_resource_name = "${var.tag_resource_name}"
-  tag_project_name = "${var.tag_project_name}"
-  tag_environment = "${var.tag_environment}"
-  tag_cost_center = "${var.tag_cost_center}"
-  tag_tier = "${var.tag_tier}"
-  tag_app_operations_owner = "${var.tag_app_operations_owner}"
-  tag_system_owner = "${var.tag_system_owner}"
-  tag_budget_owner = "${var.tag_budget_owner}"
-}
+//module "vpc_peering" {
+//  source = "../../modules/vpc-peering"
+//
+//  mgmt_vpc_id = "${var.mgmt_vpc_id}"
+//  mgmt_vpc_cidr = "${var.mgmt_vpc_cidr}"
+//  mgmt_vpc_shortname = "${var.mgmt_vpc_shortname}"
+//
+//  vpc_id = "${lookup(module.vpc.vpc_outputs, "vpc_id")}"
+//  vpc_cidr = "${var.vpc_cidr_block}"
+//  vpc_shortname = "${var.vpc_shortname}"
+//
+//  dmz_route_tables = "${lookup(module.vpc.vpc_outputs, "dmz_rt_id")}"
+//  public_route_tables = "${lookup(module.vpc.vpc_outputs, "public_rt_id")}"
+//  private_route_tables = "${lookup(module.vpc.vpc_outputs, "private_rt_id")}"
+//
+//  deploy_environment = "${var.deploy_environment}"
+//
+//  tag_resource_name = "${var.tag_resource_name}"
+//  tag_project_name = "${var.tag_project_name}"
+//  tag_environment = "${var.tag_environment}"
+//  tag_cost_center = "${var.tag_cost_center}"
+//  tag_tier = "${var.tag_tier}"
+//  tag_app_operations_owner = "${var.tag_app_operations_owner}"
+//  tag_system_owner = "${var.tag_system_owner}"
+//  tag_budget_owner = "${var.tag_budget_owner}"
+//}
