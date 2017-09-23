71a690efd0ee085289ce0e3081d1a5e1702d69fe
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-23 07:22:25 +0100

    jenkins fix

--- terraform/infrastructure/account-infrastructure/10-inputs-required.tf
+++ terraform/infrastructure/account-infrastructure/10-inputs-required.tf
@@ -6,10 +6,8 @@ variable "account_id" {}
 # Jenkins CodeBuild
 variable "jenkins_codebuild_name" {}
 variable "jenkins_codebuild_repo" {}
-variable "jenkins_codebuild_ecr" {}
 variable "jenkins_image_name" {}
 
 variable "jenkins_slave_codebuild_name" {}
 variable "jenkins_slave_codebuild_repo" {}
-variable "jenkins_slave_codebuild_ecr" {}
 variable "jenkins_slave_image_name" {}
