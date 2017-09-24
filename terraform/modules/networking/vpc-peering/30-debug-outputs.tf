91211d7c271f344998074a96660ba46791e6d053
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-24 17:24:43 +0100

    bug fixes

--- terraform/modules/networking/vpc-peering/30-debug-outputs.tf
+++ terraform/modules/networking/vpc-peering/30-debug-outputs.tf
@@ -22,10 +22,6 @@ data "null_data_source" "debug_outputs" {
 
     # interpolated-defaults
     name_prefix = "${lookup(data.null_data_source.vpc_defaults.inputs, "name_prefix")}"
-    mgmt_subnets = "${data.aws_subnet_ids.mgmt_subnets.id}"
-    mgmt_dmz_route_tables = "${data.aws_route_table.mgmt_dmz_route_tables.*.id}"
-    mgmt_private_route_tables = "${data.aws_route_table.mgmt_private_route_tables.*.id}"
-    mgmt_public_route_tables = "${data.aws_route_table.mgmt_public_route_tables.*.id}"
   }
 }
 
