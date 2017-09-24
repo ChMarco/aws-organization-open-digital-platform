d25fb5fe8f32c9cb2f1061e4cbc7c09815cdd61a
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-24 14:34:36 +0100

    grafana fixes

--- terraform/modules/services/monitoring/73-grafana.tf
+++ terraform/modules/services/monitoring/73-grafana.tf
@@ -1,18 +1,18 @@
 resource "null_resource" "grafana_test" {
   provisioner "local-exec" {
-    command = "until $(curl --output /dev/null --silent --head --fail http://${lookup(module.monitoring.monitoring_outputs, "monitoring_grafana_elb_dns_name")}:3000/); do printf '.' sleep 5; done"
+    command = "until $(curl --output /dev/null --silent --head --fail http://${aws_elb.monitoring_grafana_elb.dns_name}:3000/); do printf '.' sleep 5; done"
   }
 }
 
 provider "grafana" {
-  url = "http://${lookup(module.monitoring.monitoring_outputs, "monitoring_grafana_elb_dns_name")}:3000"
+  url = "http://${aws_elb.monitoring_grafana_elb.dns_name}:3000"
   auth = "admin:changeme"
 }
 
 resource "grafana_data_source" "prometheus" {
   name = "Prometheus"
   type = "prometheus"
-  url = "http://${lookup(module.monitoring.monitoring_outputs, "monitoring_internal_elb_dns_name")}:9090/"
+  url = "http://${aws_elb.monitoring_internal_elb.dns_name}:9090/"
   basic_auth_enabled = true
   basic_auth_username = "admin"
   basic_auth_password = "changeme"
