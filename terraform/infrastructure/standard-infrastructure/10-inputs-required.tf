0e92d5d968f2f789fb8b726cedaf99534c22c119
Mohammed Abubakar <mohammed.abubakar@altostack.io>
2017-09-24 17:10:57 +0100

    standard infra

--- terraform/infrastructure/standard-infrastructure/10-inputs-required.tf
+++ terraform/infrastructure/standard-infrastructure/10-inputs-required.tf
@@ -1,6 +1,8 @@
 # General
 variable "aws_region" {}
+variable "management_keypair" {}
 variable "deploy_environment" {}
+variable "ssh_web_whitelist" {}
 
 # VPC
 variable "vpc_description" {}
@@ -10,11 +12,9 @@ variable "dmz_subnet_cidr_blocks" {}
 variable "public_subnet_cidr_blocks" {}
 variable "private_subnet_cidr_blocks" {}
 
-variable "tag_resource_name" {}
 variable "tag_project_name" {}
 variable "tag_environment" {}
 variable "tag_cost_center" {}
-variable "tag_tier" {}
 variable "tag_app_operations_owner" {}
 variable "tag_system_owner" {}
 variable "tag_budget_owner" {}
@@ -22,16 +22,3 @@ variable "tag_budget_owner" {}
 # VPC Peer
 variable "mgmt_vpc_id" {}
 variable "mgmt_vpc_cidr" {}
-
-# Bastion
-variable "bastion_public_key" {}
-variable "bastion_ssh_whitelist" { type = "map" }
-variable "provisioner_ssh_public_key" {}
-variable "tag_bastion_resource_name" {}
-variable "tag_bastion_project_name" {}
-variable "tag_bastion_environment" {}
-variable "tag_bastion_cost_center" {}
-variable "tag_bastion_service" {}
-variable "tag_bastion_app_operations_owner" {}
-variable "tag_bastion_system_owner" {}
-variable "tag_bastion_budget_owner" {}
